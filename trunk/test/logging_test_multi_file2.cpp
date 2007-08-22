e2bb4cff38e6c46ee46d118519425e3c886f1c38
jean.daniel.michaud <jean.daniel.michaud@40049854-0235-0410-b64c-117f8b557921>
2007-08-22 20:03:54 +0000

    Working qualifier example.

--- trunk/test/logging_test_multi_file2.cpp
+++ trunk/test/logging_test_multi_file2.cpp
@@ -5,8 +5,8 @@
 //  appears in all copies. This software is provided "as is" without express or 
 //  implied warranty, and with no claim as to its suitability for any purpose.
 
-//  See http://www.boost.org for updates, documentation, and revision history.
-//  See http://www.boost.org/libs/logging/ for library home page.
+//  See http://www.boost.org/LICENSE_1_0.txt for licensing.
+//  See http://code.google.com/p/loglite/ for library home page.
 
 #include <logging.hpp>
 
