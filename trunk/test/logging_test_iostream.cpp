91f1c5def13664d65c77d14746e1e8a185e66778
jean.daniel.michaud <jean.daniel.michaud@40049854-0235-0410-b64c-117f8b557921>
2007-09-25 14:17:17 +0000

    Adding test for boost::iostreams

---
+++ trunk/test/logging_test_iostream.cpp
@@ -0,0 +1,42 @@
+//  Boost general library logging_test_xml_output.cpp file  ------------------//
+
+//  (C) Copyright Jean-Daniel Michaud 2007. Permission to copy, use, modify, 
+//  sell and distribute this software is granted provided this copyright notice 
+//  appears in all copies. This software is provided "as is" without express or 
+//  implied warranty, and with no claim as to its suitability for any purpose.
+
+//  See http://www.boost.org/LICENSE_1_0.txt for licensing.
+//  See http://code.google.com/p/loglite/ for library home page.
+
+#include <ostream>
+#include <logging.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/file_descriptor.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/gzip.hpp>
+
+using namespace boost::logging;
+
+int main(int argc, char **argv)
+{
+  BOOST_LOG_INIT(trace >> eol);
+
+  boost::iostreams::filtering_ostream *out = new boost::iostreams::filtering_ostream();
+  out->push(boost::iostreams::gzip_compressor());
+  out->push(boost::iostreams::file_sink("log.gz"));
+
+  //boost::iostreams::stream_buffer<boost::iostreams::file_sink> buf("log.txt");
+
+  //sink sink_file(new std::ostream(&buf), 2);
+  sink sink_file(out, 2);
+  sink_file.attach_qualifier(boost::logging::log);
+  BOOST_LOG_ADD_OUTPUT_STREAM(sink_file);
+
+  BOOST_LOG_(1, "Strange women lying in ponds distributing swords");
+  BOOST_LOG_(1, "is no basis for a system of government");
+  BOOST_LOG_(1, "Supreme executive power derives from a mandate of the masses");
+  BOOST_LOG_(1, "not from some farcical aquatic ceremony!");
+
+  return 0;
+}
