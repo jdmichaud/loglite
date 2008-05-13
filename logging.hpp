37985e27be10d729c7fd52c645d328e2c05d3fcf
jean.daniel.michaud <jean.daniel.michaud@40049854-0235-0410-b64c-117f8b557921>
2008-05-13 17:31:25 +0000

    Fix for Issue 18 by Igor Solovyov

--- logging.hpp
+++ logging.hpp
@@ -1,6 +1,6 @@
-//  loglite library logging.hpp header file  ----------------------------------/
+//  loglite library logging.hpp header file  ---------------------------------/
 
-//  (C) Copyright Jean-Daniel Michaud, Ilya Pokolev 2007. 
+//  (C) Copyright Jean-Daniel Michaud, Ilya Pokolev & Igor Solovyov 2007-2008. 
 //  Permission to copy, use, modify, sell and distribute this software is 
 //  granted provided this copyright notice appears in all copies. 
 //  This software is provided "as is" without express or implied warranty, 
@@ -41,8 +41,11 @@
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/date_time/local_time/local_time.hpp>
 
+#ifdef _MSC_VER
+# define __func__ __FUNCTION__
+# define __PRETTY_FUNCTION__ __FUNCSIG__
 // Code copied from redhat website
-#if __STDC_VERSION__ < 199901L
+#elif __STDC_VERSION__ < 199901L
 # if __GNUC__ >= 2
 #  define __func__ __FUNCTION__
 # else
