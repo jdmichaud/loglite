5da7144eaa2af7474499d1c49643b28ec5612b77
jean.daniel.michaud <jean.daniel.michaud@40049854-0235-0410-b64c-117f8b557921>
2010-11-21 19:35:48 +0000

    Fixes for boost 1.45

--- trunk/test/zlib.cpp
+++ trunk/test/zlib.cpp
@@ -1,4 +1,5 @@
-// (C) Copyright Jonathan Turkanis 2003.
+// (C) Copyright 2008 CodeRage, LLC (turkanis at coderage dot com)
+// (C) Copyright 2003-2007 Jonathan Turkanis
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
 
@@ -13,6 +14,7 @@
 // than using it (possibly importing code).
 #define BOOST_IOSTREAMS_SOURCE 
 
+#include <boost/throw_exception.hpp>
 #include <boost/iostreams/detail/config/dyn_link.hpp>
 #include <boost/iostreams/filter/zlib.hpp> 
 #include "zlib.h"   // Jean-loup Gailly's and Mark Adler's "zlib.h" header.
@@ -69,7 +71,7 @@ zlib_error::zlib_error(int error)
     : BOOST_IOSTREAMS_FAILURE("zlib error"), error_(error) 
     { }
 
-void zlib_error::check(int error)
+void zlib_error::check BOOST_PREVENT_MACRO_SUBSTITUTION(int error)
 {
     switch (error) {
     case Z_OK: 
@@ -77,9 +79,9 @@ void zlib_error::check(int error)
     //case Z_BUF_ERROR: 
         return;
     case Z_MEM_ERROR: 
-        throw std::bad_alloc();
+        boost::throw_exception(std::bad_alloc());
     default:
-        throw zlib_error(error);
+        boost::throw_exception(zlib_error(error));
         ;
     }
 }
@@ -89,7 +91,7 @@ void zlib_error::check(int error)
 namespace detail {
 
 zlib_base::zlib_base()
-    : stream_(new z_stream), calculate_crc_(false), crc_(0)
+    : stream_(new z_stream), calculate_crc_(false), crc_(0), crc_imp_(0)
     { }
 
 zlib_base::~zlib_base() { delete static_cast<z_stream*>(stream_); }
@@ -119,7 +121,7 @@ void zlib_base::after(const char*& src_begin, char*& dest_begin, bool compress)
             static_cast<zlib::uint>(next_in - src_begin) :
             static_cast<zlib::uint>(next_out - dest_begin);
         if (length > 0)
-            crc_ = crc32(crc_, buf, length);
+            crc_ = crc_imp_ = crc32(crc_imp_, buf, length);
     }
     total_in_ = s->total_in;
     total_out_ = s->total_out;
@@ -127,12 +129,12 @@ void zlib_base::after(const char*& src_begin, char*& dest_begin, bool compress)
     dest_begin = next_out;
 }
 
-int zlib_base::deflate(int flush)
+int zlib_base::xdeflate(int flush)
 { 
     return ::deflate(static_cast<z_stream*>(stream_), flush);
 }
 
-int zlib_base::inflate(int flush)
+int zlib_base::xinflate(int flush)
 { 
     return ::inflate(static_cast<z_stream*>(stream_), flush);
 }
@@ -142,18 +144,19 @@ void zlib_base::reset(bool compress, bool realloc)
     z_stream* s = static_cast<z_stream*>(stream_);
     // Undiagnosed bug:
     // deflateReset(), etc., return Z_DATA_ERROR
-    //zlib_error::check(
+    //zlib_error::check BOOST_PREVENT_MACRO_SUBSTITUTION(
         realloc ?
             (compress ? deflateReset(s) : inflateReset(s)) :
             (compress ? deflateEnd(s) : inflateEnd(s))
                 ;
     //);
+    crc_imp_ = 0;
 }
 
 void zlib_base::do_init
     ( const zlib_params& p, bool compress, 
       #if !BOOST_WORKAROUND(BOOST_MSVC, < 1300)
-          zlib::alloc_func alloc, zlib::free_func free, 
+          zlib::xalloc_func /* alloc */, zlib::xfree_func /* free*/, 
       #endif
       void* derived )
 {
@@ -171,7 +174,7 @@ void zlib_base::do_init
     //#endif
     s->opaque = derived;
     int window_bits = p.noheader? -p.window_bits : p.window_bits;
-    zlib_error::check(
+    zlib_error::check BOOST_PREVENT_MACRO_SUBSTITUTION(
         compress ?
             deflateInit2( s, 
                           p.level,
